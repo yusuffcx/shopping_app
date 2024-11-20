b6cd8a9c6498af7afb8c74869fafc5e629eb4ed6
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-11-20 13:08:26 +0300

    added models and data

---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,14 @@
+import 'package:shopping_list/models/category.dart';
+
+class GroceryItem {
+  const GroceryItem(
+      {required this.category,
+      required this.id,
+      required this.name,
+      required this.quantity});
+
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
